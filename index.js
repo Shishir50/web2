88bbc348469d943690924cb3cf9a1586bc8b648b
Shishir Thakur <90712829+Shishir50@users.noreply.github.com>
2022-11-22 18:00:55 +0530

    files added

---
+++ index.js
@@ -0,0 +1,10 @@
+function slidebar(){
+    document.getElementById('slidenav').style.display = 'block'
+    document.getElementById('body').style.display = 'none'
+}
+function closebar(){
+    
+    document.getElementById('slidenav').style.display = 'none'
+    document.getElementById('body').style.display = 'block'
+}
+
